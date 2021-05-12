38a8fe28c4a631eae1ad2373c5be4edd50516469
ferdous85 <ferdous85@gmail.com>
2021-05-12 09:41:54 +0600

    db connect

---
+++ config/connectDB.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const {Sequelize} = require('sequelize')
+
+const DB_NAME= process.env.DB_NAME
+const DB_USERNAME= process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
